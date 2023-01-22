11c0c9f1d1ff485ad40b7f30f08bd5a2c79a8278
Aduwo Ayooluwa <aduwoayooluwa@gmail.com>
2023-01-22 08:58:55 +0100

    working on api fetching and getting access token

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,24 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [token, setToken] = useState("")
+
+  const fetchSpot =async () => {
+    return axios.get('https://api.spotify.com/v1/me', {
+      headers: {
+        'Authorization': 'b535bd2f0af342d6a82b68b7ed645860'
+      }
+    })
+  }
+  const { data, isLoading, isError } = useQuery({queryKey: ["app"], queryFn: fetchSpot,})
+
+  if (isError) return <>Error Loading Page</>
   return (
     <>
       <Head>
@@ -17,7 +31,15 @@ export default function Home() {
       </Head>
       <main className=''>
 
-
+        {
+          isLoading ? (<>
+            Loading....
+          </>) : (
+            <>
+              {data}
+            </>
+          )
+        }
       </main>
     </>
   )
