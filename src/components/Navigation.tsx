5c9f4274f788fa29b114880ed4236299fedbaf86
Aduwo Ayooluwa <aduwoayooluwa@gmail.com>
2023-01-22 11:32:42 +0100

    working on the bavigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Navigation = () => {
     return (
-        <div>Navigation</div>
+        <nav className='grid grid-col place-content-center'>
+            <section className='flex w-full md:w-[800px] justify-between bg-[#3f3046] py-3 px-10'>
+                <p className='font-medium text-lg'> Spotify </p>
+
+                <div>
+                    <select className='fonr-normal text-lg'>
+                        <option>Encryption</option>
+                        <option>Account</option>
+                        <option>Profile</option>
+                        <option>Private Session</option>
+                        <option>Settings</option>
+                    </select>
+                </div>
+            </section>
+        </nav>
     )
 }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import Navigation from '@/components/Navigation'
 
 export default function App({ Component, pageProps }: AppProps) {
 
   const queryClient = new QueryClient()
   return (
   <QueryClientProvider client={queryClient}>
+    <Navigation />
     <Component {...pageProps} />
   </QueryClientProvider>
   )
